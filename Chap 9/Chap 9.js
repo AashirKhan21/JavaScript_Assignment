d0e9058e38b7a0de60f22ff1555760e35d31a796
Muhammad Aashir Khan <55949359+AashirKhan21@users.noreply.github.com>
2020-01-01 19:59:39 +0500

    Push JS Assignment Chapter 9

---
+++ Chap 9/Chap 9.js
@@ -0,0 +1,4 @@
+var firstName = prompt("Please Enter Your First Name", "Aashir");
+var lastName = prompt("Please Enter Your Last Name", "Khan");
+var age = prompt("Please Enter Your Age");
+alert("Hello "+firstName+" "+lastName+", You are "+age+" years old.");
