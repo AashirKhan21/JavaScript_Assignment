a4b69905a9689f9d92d382f359aa29b19be0403b
Muhammad Aashir Khan <55949359+AashirKhan21@users.noreply.github.com>
2020-01-01 20:08:10 +0500

    Push JS Assignment Chap 18 to 20

---
+++ Chap 18 to 20/Chap 18 to 20.js
@@ -0,0 +1,12 @@
+var courses=[]
+for(i=0 ; i <=4; i++){
+    courses[i] = prompt("Enter your subject.")
+    }
+    alert(courses);
+    for(i=0 ; i<= 4 ; i++){
+        alert(courses[i])
+    }
+    for(i=0 ; i<= 4 ; i++){
+    courses[i] = prompt("Please Enter Your subject again.");
+}
+alert(courses); 
